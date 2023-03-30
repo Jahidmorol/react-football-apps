8e274c7a5846bf67217f4487ffca6106485779f7
jahid morol <jahidmorol2@gmail.com>
2023-03-30 17:39:03 +0600

    everything is fine and almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,67 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
-import './App.css'
-import Header from './component/Header/Header'
-import Summary from './component/Summary/Summary'
-import Team from './component/Team/Team'
-import { addToDb } from './utilities/fakeDb'
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
+import Header from "./component/Header/Header";
+import Summary from "./component/Summary/Summary";
+import Team from "./component/Team/Team";
+import { addToDb, getSummaryCart } from "./utilities/fakeDb";
 
 function App() {
-  const [teams, setTeams] = useState([])
-  const [cart, setCart] = useState([])
-  useEffect( ()=>{
-    fetch('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League')
-    .then(res => res.json())
-    .then(data => setTeams(data.teams))
-  },[])
+  const [teams, setTeams] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League"
+    )
+      .then((res) => res.json())
+      .then((data) => setTeams(data.teams));
+  }, []);
 
-const handleAddTeam = (team) => {
-  const newCart = [...cart, team]
-  // console.log([...cart, team]);
-  setCart(newCart)
-  addToDb(team.idTeam)
-  // console.log('clicked',team);
-}
+  const handleAddTeam = (team) => {
+    const newCart = [...cart, team];
+    // console.log([...cart, team]);
+    setCart(newCart);
+    addToDb(team.idTeam);
+    // console.log('clicked',team);
+  };
+  useEffect(() => {
+    const storedCart = getSummaryCart();
+    console.log(storedCart);
+    console.log(teams);
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedTeam = teams.find((team) => team.idTeam == id);
+      if (addedTeam) {
+        addedTeam.quantity = storedCart[id]
+        savedCart.push(addedTeam);
+      }
+
+    }
+    console.log(savedCart);
+    setCart(savedCart);
+  }, [teams]);
+  // console.log(cart);
   return (
     <div className="App">
       <header>
         <Header></Header>
       </header>
-      <main className='football-container'>
-        <div className='football-info'>
-          {
-            teams.map(team => <Team 
-              key={team.idTeam} 
-              team={team} 
+      <main className="football-container">
+        <div className="football-info">
+          {teams.map((team) => (
+            <Team
+              key={team.idTeam}
+              team={team}
               handleAddTeam={handleAddTeam}
-            ></Team>)
-          }
+            ></Team>
+          ))}
         </div>
-        <div className='football-summery'>
-            <Summary cart={cart}></Summary>
+        <div className="football-summery">
+          <Summary cart={cart}></Summary>
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Summary/Summary.jsx
+++ src/component/Summary/Summary.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import './Summery.css'
 
 const Summary = ({cart}) => {
-    // console.log(cart);
+    console.log(cart);
+    let quantity = 0;
+    let nTeam = {};
+    for(const team of cart){
+        // if(team.quantity === 0){
+        //         team.quantity = 1;
+        //     }
+        team.quantity = team.quantity || 1;
+        quantity = quantity + team.quantity;
+        nTeam = team;
+        // document.getElementById('all-img').innerHTML=`
+        // <img src=${nTeam.strTeamBanner} alt="photo" />
+        // `;
+    }
+    
     return (
         <div className='summary'>
             <h2>Football Summary</h2>
-            <h4>Total Add: {cart.length}</h4>
-
+            <h4>Total Add: {quantity}</h4>
+            <img src={nTeam.strTeamBanner} alt="" />
+            {/* .reduce((p,t)=> t?.quantity || 0 + p, 0) */}
         </div>
     );
 };
